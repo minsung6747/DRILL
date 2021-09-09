46c3efe9e6c1f842bbc6562f60b52f7def06f06f
minsung6747 <miin_sung@naver.com>
2021-09-09 20:46:07 +0900

    DRILL04 모눈 그리기
    2018180006 김민성

---
+++ DRILL04/grid.py
@@ -0,0 +1,42 @@
+Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import turtle
+>>> turtle.reset()
+>>> count=6
+>>> while(count>0):
+	turtle.forward(500)
+	turtle.penup()
+	turtle.back(500)
+	turtle.left(90)
+	turtle.forward(100)
+	turtle.pendown()
+	turtle.right(90)
+	count=count-1
+
+	
+>>> turtle.goto(0,0)
+>>> count
+0
+>>> turtle.undo()
+>>> turtle.penup()
+>>> turtle.goto(0,0)
+>>> turtle.left(90)
+>>> cnt=6
+>>> while(cnt>0):
+	turtle.forward(500)
+	turtle.penup()
+	turtle.back(500)
+	turtle.right(90)
+	turtle.pendown()
+	turtle.forward(100)
+	turtle.left(90)
+	cnt=cnt-1
+
+	
+>>> turtle.undo()
+>>> turtle.undo()
+>>> turtle.goto(0,0)
+>>> turtle.left(90)
+]
+>>> turtle.forward(500)
+>>> turtle.exitonclick()
